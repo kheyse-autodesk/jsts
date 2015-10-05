d3571060ccef6e6e8a8455817af547363f898a84
Björn Harrtell <bjorn.harrtell@sweco.se>
2015-10-05 15:07:32 +0200

    Naive impl

--- src/java/util/LinkedList.js
+++ src/java/util/LinkedList.js
@@ -1,1 +1,14 @@
-export default class LinkedList {}
+export default class LinkedList {
+  constructor() {
+    this.array_ = []
+  }
+  addLast(e) {
+    this.array_.push(e)
+  }
+  removeFirst() {
+    return this.array_.shift()
+  }
+  isEmpty() {
+    return this.array_.length === 0
+  }
+}
