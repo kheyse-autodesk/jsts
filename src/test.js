22c9cac8c361b716b2754c376e15f1be04ebe5f8
Björn Harrtell <bjorn@wololo.org>
2015-10-12 21:26:46 +0200

    Test code from JTS

--- src/test.js
+++ src/test.js
@@ -1,6 +1,8 @@
 var WKTReader = require('./com/vividsolutions/jts/io/WKTReader')
 var WKTWriter = require('./com/vividsolutions/jts/io/WKTWriter')
 var BufferOp = require('./com/vividsolutions/jts/operation/buffer/BufferOp')
+var OverlayOp = require('./com/vividsolutions/jts/operation/overlay/OverlayOp')
+var RelateOp = require('./com/vividsolutions/jts/operation/relate/RelateOp')
 
 var reader = new WKTReader()
 var writer = new WKTWriter()
@@ -10,14 +12,22 @@ var l1 = reader.read('LINESTRING(1 1, 1 2)')
 var r1 = reader.read('LINEARRING(1 1, 1 2, 3 3, 1 1)')
 var a1 = reader.read('POLYGON(1 1, 1 2, 3 3, 1 1)')
 
-/*var RelateOp = require('com/vividsolutions/jts/operation/relate/RelateOp')
+/*
 var intersects = RelateOp.intersects(p1, a1)
-console.log(intersects)*/
+console.log(intersects)
+*/
 
 var buffer = BufferOp.bufferOp(l1, 10)
-
 console.log(writer.write(buffer))
 
-/*var OverlayOp = require('com/vividsolutions/jts/operation/overlay/OverlayOp')
+/*
 var intersection = OverlayOp.intersection(l1, a1)
-console.log(intersection)*/
+console.log(intersection)
+*/
+
+var a = reader.read('LINESTRING(240 190, 120 120)')
+var b = reader.read('POLYGON((110 240, 50 80, 240 70, 110 240))')
+var intersection = OverlayOp.intersection(a, b)
+console.log(writer.write(intersection))
+// expected: LINESTRING(177 153, 120 120)
+
